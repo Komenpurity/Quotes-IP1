06f245c15b24e7df16596171218f65ccb7a5ccc3
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2022-04-11 09:02:33 +0300

    added form,quote and vote component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './component/quote/quote.component';
+import { FormQuoteComponent } from './component/form-quote/form-quote.component';
+import { VoteComponent } from './component/vote/vote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    FormQuoteComponent,
+    VoteComponent
   ],
   imports: [
     BrowserModule
