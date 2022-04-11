5263f254b4938dafd892ed65a45f68dcc659bbf9
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2022-04-11 10:28:14 +0300

    submit button functional

--- src/app/component/form-quote/form-quote.component.ts
+++ src/app/component/form-quote/form-quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from 'src/app/quotes';
 
 @Component({
   selector: 'app-form-quote',
@@ -7,12 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormQuoteComponent implements OnInit {
 
+  upVote: number = 0;
+  downVote: number = 0;
+  time!: Date;
   header: string = "Fill in the Quote"
   quoteName!: string;
   quoteMessage!: string;
   quoteAuthor!: string;
+  newMessage: any;
   
   constructor() { }
+  
+  messages: Quotes[] = [
+    new Quotes("Mark John","Winter is Coming","Jon Snow",0,0,'01-10-2015')
+
+  ];
+
+
+  submitQuote() {
+    this.newMessage= new Quotes(this.quoteName,this.quoteMessage,this.quoteAuthor,this.upVote,this.downVote,this.time);
+    this.messages.push(this.newMessage);
+  }
 
   ngOnInit(): void {
   }
