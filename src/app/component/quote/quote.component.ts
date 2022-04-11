1c5f5988e593e0b208cf0f8b379610050910ddbb
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2022-04-11 09:48:03 +0300

    haeding complete

--- src/app/component/quote/quote.component.ts
+++ src/app/component/quote/quote.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
+  heading: string = "Quotes App"
+
   constructor() { }
 
   ngOnInit(): void {
